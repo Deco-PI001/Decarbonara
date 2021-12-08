c65b924c913ce534ece1094b5fee6ecaafbb7ce9
Karsten <Karsten@nas01.local>
2021-12-08 20:09:36 +0100

    Test 1 neuer Laptop

--- Decarbonara/Python/Auswertung_rpi_log.py
+++ Decarbonara/Python/Auswertung_rpi_log.py
@@ -1,26 +1,28 @@
 #from typing import Collection
 import mysql.connector
 import matplotlib.pyplot as plt
-import numpy as np
-from datetime import datetime
+#import numpy as np
+import os
+from datetime import *
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+    ## DAS IST
+    ## VOM 
+    ## NEUEN
+    ## Lptop
     
-aktuelle_zeit = datetime.now().strftime('%Y%m%d-%H%M%S')  
-                            # Format = 20211109-131856
-
-i_fenster = tk.Tk()
-i_fenster.title('Eingaben für die Auswertungsgrafik')
-i_fenster.geometry('1210x570+10+10')
 
 def button_action():
 
     p_server_name = p1.get()
     p_etage = p2.get()
     p_raum = p3.get()
-    p_name_des_wertes = p4.get()
+    p_sensorklasse = p4.get()
+    p_name_des_wertes = p5.get()
+    p_datum_von = p6.get()
+    p_datum_bis = p7.get()
 
     if p_server_name == '':
         fehler.set('Bitte wählen Sie einen gültigen Server aus.')
@@ -30,7 +32,7 @@ def button_action():
         fehler.set('Bitte wählen Sie einen gültigen Raum aus.')
     else:
         fehler.set('')
-        x_grafik = graf_erzeugen(p_server_name, p_etage, p_raum, p_name_des_wertes)
+        x_grafik = graf_erzeugen(p_server_name, p_etage, p_raum, p_sensorklasse, p_name_des_wertes, p_datum_von, p_datum_bis)
         n_grafik = ImageTk.PhotoImage(Image.open(x_grafik))
         l_grafik .configure(image=n_grafik)
         l_grafik.image = n_grafik
@@ -38,20 +40,20 @@ def button_action():
         #i_fenster.destroy()
 
 
-def graf_erzeugen(p_server_name, p_etage, p_raum, p_name_des_wertes):
-
-    db_verbindung = mysql.connector.connect(database='DeCarbonara',
-                                                user='root',
-                                            password='DeCarbonaras#2021') 
-                                            #password=getpass("Enter password: "))
-    cursor = db_verbindung.cursor()
+def graf_erzeugen(p_server_name, p_etage, p_raum, p_sensorklasse, p_name_des_wertes, p_datum_von, p_datum_bis):
 
     try:
+        sql_time_min = time.min.__str__()
+        sql_datum_von =  p_datum_von + ' ' + sql_time_min
+        sql_time_max = time.max.__str__()
+        sql_datum_bis =  p_datum_bis + ' ' + sql_time_max
         sql_anweisung = """SELECT * FROM import_messwerte 
-                        WHERE server_name = %s AND etage = %s AND raum = %s AND name_des_wertes = %s
+                        WHERE server_name = %s AND etage = %s AND raum = %s AND sensorklasse = %s AND name_des_wertes = %s 
+                              AND log_datum_vom >= DATE(%s) AND log_datum_vom <= DATE(%s)
                         ORDER BY log_datum_vom"""
-        sql_werte = (p_server_name, p_etage, p_raum, p_name_des_wertes)
+        sql_werte = (p_server_name, p_etage, p_raum, p_sensorklasse, p_name_des_wertes, sql_datum_von, sql_datum_bis)
              
+        cursor = db_verbindung.cursor()
         cursor.execute(sql_anweisung, sql_werte)
         sql_ergebnis = cursor.fetchall()
         cursor.close()
@@ -65,28 +67,68 @@ def graf_erzeugen(p_server_name, p_etage, p_raum, p_name_des_wertes):
             xwerte.append(xwerte_init)
             ywerte_init = [daten[7]]
             ywerte.append(ywerte_init)
-            #print('Zeit: ' + str(daten[1]))
-            #print('wert_num: ' + str(daten[7]))
+            print('Zeit: ' + str(daten[1]))
+            print('wert_num: ' + str(daten[7]))
 
     except mysql.connector.Error as error:
         print("Fehler beim Lesen der Tabelle import_messwerte: {}".format(error))
 
     plt.plot(xwerte, ywerte) #, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=12)
-    #plt.bar(xwerte, ywerte)
+    #plt.bar(xwerte, ywerte)pyth
     plt.xlabel('Zeit-Achse')
     plt.ylabel('Temperatur')
+    
     s_grafik = aktuelle_zeit + '_' + p_server_name + '_' + p_etage + '_' + p_raum + '.png'
     plt.savefig(s_grafik)
     #plt.show()
+    
     return s_grafik
 
 
-l_server_name = Label(i_fenster, text="Server: ", anchor='w').place(x=20, y=50, width=50, height=24)
-l_etage = Label(i_fenster, text="Etage: ", anchor='w').place(x=20, y=80, width=50, height=24)
-l_raum = Label(i_fenster, text="Raum: ", anchor='w').place(x=20, y=110, width=50, height=24)
-l_name_des_wertes = Label(i_fenster, text="Wert: ", anchor='w').place(x=20, y=140, width=50, height=24)
+def db_select(p_sql_anweisung, p_sql_werte, p_sql_tabelle):
+    
+    r_sql_ergebnis = None
+    try:
+        cursor = db_verbindung.cursor()
+        cursor.execute(p_sql_anweisung, p_sql_werte)
+        r_sql_ergebnis = cursor.fetchall()
+        cursor.close()
+    except mysql.connector.Error as error:
+        print("Fehler beim Lesen der Tabelle " + p_sql_tabelle + " : {}".format(error))
+    
+    return r_sql_ergebnis
+
+
+aktuelle_zeit = datetime.now().strftime('%Y%m%d-%H%M%S')  
+                            # Format = 20211109-131856
+
+db_verbindung = mysql.connector.connect(database='DeCarbonara',
+                                            user='root',
+                                        password='DeCarbonaras#2021') 
+                                       #password=getpass("Enter password: "))
+cursor = db_verbindung.cursor()
+
+
+i_fenster = tk.Tk()
+i_fenster.title('Eingaben für die Auswertungsgrafik')
+i_fenster.geometry('1210x570+10+10')
+
+arbeits_pfad = os.getenv('HOME') + '/Alles/Kunden/Decarbonara_GIT/Decarbonara/Python'
+os.chdir(arbeits_pfad)
+
+l_eckdaten = Label(i_fenster, text="Eckdaten", anchor='w', font=('Arial', 18)).place(x=20, y=50, width=100, height=24)
+l_server_name = Label(i_fenster, text="Server: ", anchor='w').place(x=20, y=90, width=100, height=24)
+l_etage = Label(i_fenster, text="Etage: ", anchor='w').place(x=20, y=120, width=100, height=24)
+l_raum = Label(i_fenster, text="Raum: ", anchor='w').place(x=20, y=150, width=100, height=24)
+l_sensorklasse = Label(i_fenster, text="Sensorklasse: ", anchor='w').place(x=20, y=180, width=100, height=24)
+l_name_des_wertes = Label(i_fenster, text="Wert: ", anchor='w').place(x=20, y=210, width=100, height=24)
+
+l_zeitraum = Label(i_fenster, text="Zeitraum", anchor='w', font=('Arial', 18)).place(x=20, y=270, width=100, height=24)
+l_zeitraum_von = Label(i_fenster, text="Datum von: ", anchor='w').place(x=20, y=310, width=100, height=24)
+l_zeitraum_bis = Label(i_fenster, text="Datum bis: ", anchor='w').place(x=20, y=350, width=100, height=24)
+
 fehler = tk.StringVar()
-l_fehlerzeile = Label(i_fenster, text="hier", fg='red', textvariable=fehler, anchor='w').place(x=20, y=260, width=400, height=24)
+l_fehlerzeile = Label(i_fenster, text="hier", fg='red', textvariable=fehler, anchor='w').place(x=20, y=480, width=400, height=24)
 
 s_grafik= ImageTk.PhotoImage(Image.open("leer_grafik.png"))
 l_grafik = Label(i_fenster, image=s_grafik)
@@ -94,43 +136,67 @@ l_grafik.place(x=530, y=50)
 
 p1 = tk.StringVar()
 cb_server_name = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p1)
-cb_server_name['values'] = ('PI001',
-                            'PI002',
-                            'PI003')
-cb_server_name.place(x=250, y=50)
+sql_anweisung = """SELECT DISTINCT server_name FROM import_messwerte
+                   ORDER BY server_name"""
+a_server_name = db_select(sql_anweisung, None, "import_messwerte")
+cb_server_name['values'] = a_server_name
+cb_server_name.place(x=250, y=90)
 #cb_server_name.current(0)
 
 p2 = tk.StringVar()
 cb_etage = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p2)
-cb_etage['values'] = ('KG',
-                      'EG',
-                      'OG1',
-                      'DG')
-cb_etage.place(x=250, y=80)
+sql_anweisung = """SELECT DISTINCT etage FROM import_messwerte
+                   ORDER BY etage"""
+a_etage = db_select(sql_anweisung, None, "import_messwerte")
+cb_etage['values'] = a_etage
+cb_etage.place(x=250, y=120)
 #cb_etage.current(0)
 
 p3 = tk.StringVar()
 cb_raum = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p3)
-cb_raum['values'] = ('Bad',
-                     'Gäste_WC',
-                     'Schlafzimmer',
-                     'Wohnzimmer',
-                     'Balkon',
-                     'Arbeitszimmmer',
-                     'Esszimmer',
-                     'Küche',
-                     'Büro_DG')
-cb_raum.place(x=250, y=110)
+sql_anweisung = """SELECT DISTINCT raum FROM import_messwerte
+                   ORDER BY raum"""
+a_raum = db_select(sql_anweisung, None, "import_messwerte")
+cb_raum['values'] = a_raum
+cb_raum.place(x=250, y=150)
 #cb_raum.current(0)
 
 p4 = tk.StringVar()
-cb_name_des_wertes = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p4)
-cb_name_des_wertes['values'] = ('ACTUAL_TEMPERATURE',
-                                'HUMIDITY', 'CURRENT_ILLUMINATION', 'MOTION')
-cb_name_des_wertes.place(x=250, y=140)
+cb_sensorklasse = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p4)
+sql_anweisung = """SELECT DISTINCT sensorklasse FROM import_messwerte
+                   ORDER BY sensorklasse"""
+a_sensorklasse = db_select(sql_anweisung, None, "import_messwerte")
+cb_sensorklasse['values'] = a_sensorklasse
+cb_sensorklasse.place(x=250, y=180)
+#cb_raum.current(0)
+
+p5 = tk.StringVar()
+cb_name_des_wertes = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p5)
+sql_anweisung = """SELECT DISTINCT name_des_wertes FROM import_messwerte
+                   ORDER BY name_des_wertes"""
+a_name_des_wertes = db_select(sql_anweisung, None, "import_messwerte")
+cb_name_des_wertes['values'] = a_name_des_wertes
+cb_name_des_wertes.place(x=250, y=210)
 cb_name_des_wertes.current(0)
 
-ok_button = Button(i_fenster, text="Auswertungsgrafik erzeugen", command=button_action).place(x=20, y=200)
-quit_button = Button(i_fenster, text="Beenden", command=i_fenster.destroy).place(x=370, y=200)
+sql_anweisung = """SELECT DISTINCT DATE_FORMAT(log_datum_vom, '%Y-%m-%d') AS datum FROM import_messwerte
+                   GROUP BY datum 
+                   ORDER BY datum"""
+a_zeitraum = db_select(sql_anweisung, None, "import_messwerte")
+
+p6 = tk.StringVar()
+cb_zeitreaum_von = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p6)
+cb_zeitreaum_von['values'] = a_zeitraum #.strftime('%d%m%Y') 
+cb_zeitreaum_von.place(x=250, y=310)
+#cb_zeitreaum_von.current(0)
+
+p7 = tk.StringVar()
+cb_zeitraum_bis = ttk.Combobox(i_fenster, width=20, height=25, textvariable=p7)
+cb_zeitraum_bis['values'] = a_zeitraum
+cb_zeitraum_bis.place(x=250, y=350)
+#cb_zeitraum_bis.current(0)
+
+ok_button = Button(i_fenster, text="Auswertungsgrafik erzeugen", command=button_action).place(x=20, y=420)
+quit_button = Button(i_fenster, text="Beenden", command=i_fenster.destroy).place(x=370, y=420)
 
 i_fenster.mainloop()
